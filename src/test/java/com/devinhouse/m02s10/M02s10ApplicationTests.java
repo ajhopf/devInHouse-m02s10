cec16e604284d19f522308b5c09c661a6af55823
André Hopf <andre.hopf@gmail.com>
2023-04-03 20:53:55 -0300

    First commit

---
+++ src/test/java/com/devinhouse/m02s10/M02s10ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devinhouse.m02s10;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class M02s10ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
